53ba6a5f42cb025c8ce46f224487356ee4905277
Zander Maxwell <alexander.maxwell@check24.de>
2025-12-26 14:55:09 +0100

    Cookie function simplification

--- dialog-handlers.js
+++ dialog-handlers.js
@@ -1,13 +1,6 @@
 // Helper function to get cookie value
 function getCookie(name) {
-  const nameEQ = name + '=';
-  const ca = document.cookie.split(';');
-  for (let i = 0; i < ca.length; i++) {
-    let c = ca[i];
-    while (c.charAt(0) === ' ') c = c.substring(1, c.length);
-    if (c.indexOf(nameEQ) === 0) return decodeURIComponent(c.substring(nameEQ.length, c.length));
-  }
-  return null;
+  return document.cookie.match('(^|;)\\s*' + name + '\\s*=\\s*([^;]+)')?.pop() || null;
 }
 
 // Helper to close dialog on backdrop click
